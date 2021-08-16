bcf213792624d26742f923f8b7b7e82d612bf577
MansiTardeja <mansitardeja1999@gmail.com>
2021-08-16 10:05:30 +0530

    Second one

--- text_analyze.py
+++ text_analyze.py
@@ -8,6 +8,19 @@ with open("abc.txt") as f:
     text=f.read()
 count(text,"am")
 
+#What is the result of this code?
+nums = (55, 44, 33, 22)
+print(max(min(nums[:2]), abs(-42)))
+
+
+text=input()
+c=text.split()
+
+
+    
+
+           
+
 
         
 
